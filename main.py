e9efff27a9f4c5da0265317019c8cc4f9b3b80ce
ssendrowski <sendrowski.sz@gmail.com>
2025-05-21 09:27:47 +0200

    wip: added delete user function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.model import users
-from utils.controller import getUserInfo, add_user
+from utils.controller import get_user_info, add_user, delete_user
 
 def main():
     print(f'Witaj {users[0]['name']}')
@@ -8,12 +8,14 @@ def main():
         print('0 - Zakończ program')
         print('1 - Wyświetl znajomych')
         print('2 - Dodaj znajomych')
+        print('3 - Usuń znajomego')
         print('==================================')
 
         choice = input('Wybierz opcje Menu')
         if choice == '0': break
-        if choice == '1' : getUserInfo(users[1:])
+        if choice == '1' : get_user_info(users[1:])
         if choice == '2': add_user(users)
+        if choice == '3': delete_user(users)
 
 if __name__ == '__main__':
     main()
