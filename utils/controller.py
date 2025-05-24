993160070978be564526d63df7fe80b04e915e76
ssendrowski <sendrowski.sz@gmail.com>
2025-05-24 10:10:16 +0200

    wip: added get_map and get_coordinates function

--- utils/controller.py
+++ utils/controller.py
@@ -21,3 +21,24 @@ def update_user(users_data: list) -> None:
             user['name'] = input('Podaj nowe imie: ')
             user['location'] = input('Podaj nowa lokalizację: ')
             user['posts'] = input('Podaj liczbę postów: ')
+
+def get_coordinates(location_name: str) -> list:
+    import requests
+    from bs4 import BeautifulSoup
+    adres_url: str = f'https://pl.wikipedia.org/wiki/{location_name}'
+    response_html = BeautifulSoup(requests.get(adres_url).text, 'html.parser')
+    return [
+        float(response_html.select('.latitude')[1].text.replace(',', '.')),
+        float(response_html.select('.longitude')[1].text.replace(',', '.')),
+    ]
+
+def get_map(users_data: list[dict]) -> None:
+    import folium
+    map = folium.Map(location=[52.3, 21.0], zoom_start=6, tiles='OpenStreetMap')
+    for user in users_data:
+        # print(get_coordinates(user['location'])) debug
+        folium.Marker(
+            location=get_coordinates(user['location']),
+            popup=f'<h5>{user['location']}</h5><br/>{user['name']}<br/>{user['posts']}'
+        ).add_to(map)
+    map.save('mapa.html')
