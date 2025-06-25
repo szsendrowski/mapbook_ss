4133936efee8a4f29a84639905a359c591814046
ssendrowski <sendrowski.sz@gmail.com>
2025-06-26 01:38:42 +0200

    wip: crud for formularz

--- main.py
+++ main.py
@@ -1,6 +1,119 @@
 from tkinter import *
 import tkintermapview
 
+users: list = []
+
+class User:
+    def __init__(self, name, surname, location, posts, map_widget):
+        self.name = name
+        self.surname = surname
+        self.location = location
+        self.posts = posts
+        self.cordinates = self.get_cordinates()
+        self.marker = map_widget.set_marker(self.cordinates[0], self.cordinates[1],
+                                            text=f'{self.name} {self.surname}')
+
+    def get_cordinates(self) -> list:
+        from bs4 import BeautifulSoup
+        import requests
+        adress_url = f'https://pl.wikipedia.org/wiki/{self.location}'
+        response = requests.get(adress_url)
+        if response.status_code == 200:
+            response_html = BeautifulSoup(response.content, 'html.parser')
+            return [
+                float(response_html.select('.latitude')[1].text.replace(',', '.')),
+                float(response_html.select('.longitude')[1].text.replace(',', '.'))
+            ]
+
+
+def add_user() -> None:
+    name = entry_imie.get()
+    surname = entry_nazwisko.get()
+    location = entry_miejscowosc.get()
+    posts = entry_posts.get()
+
+    user = User(name=name, surname=surname, location=location, posts=posts, map_widget=map_widget)
+
+    users.append(user)
+
+    print(users)
+
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_miejscowosc.delete(0, END)
+    entry_posts.delete(0, END)
+
+    entry_imie.focus()
+
+    show_users()
+
+
+def show_users() -> None:
+    listbox_lista_obiektow.delete(0, END)
+    for idx, user in enumerate(users):
+        listbox_lista_obiektow.insert(idx, f'{idx + 1}.{user.name} {user.surname}')
+
+
+def remove_user() -> None:
+    i = listbox_lista_obiektow.index(ACTIVE)
+    users[i].marker.delete()
+    users.pop(i)
+    show_users()
+
+
+def edit_user() -> None:
+    i = listbox_lista_obiektow.index(ACTIVE)
+    name = users[i].name
+    surname = users[i].surname
+    location = users[i].location
+    posts = users[i].posts
+
+    entry_imie.insert(0, name)
+    entry_nazwisko.insert(0, surname)
+    entry_miejscowosc.insert(0, location)
+    entry_posts.insert(0, posts)
+
+    button_dodaj_obiekt.config(text='Zapisz', command=lambda: update_users(i))
+
+
+def update_users(i) -> None:
+    name = entry_imie.get()
+    surname = entry_nazwisko.get()
+    location = entry_miejscowosc.get()
+    posts = entry_posts.get()
+
+    users[i].name = name
+    users[i].surname = surname
+    users[i].location = location
+    users[i].posts = posts
+
+    users[i].cordinates = users[i].get_cordinates()
+    users[i].marker.delete()
+    users[i].marker = map_widget.set_marker(users[i].cordinates[0], users[i].cordinates[1],
+                                            text=f'{users[i].name} {users[i].surname}')
+
+    show_users()
+
+    button_dodaj_obiekt.config(text='Dodaj', command=add_user)
+
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_miejscowosc.delete(0, END)
+    entry_posts.delete(0, END)
+
+    entry_imie.focus()
+
+
+def show_user_details():
+    i = listbox_lista_obiektow.index(ACTIVE)
+    label_szczegoly_obiektu_name_wartosc.config(text=users[i].name)
+    label_szczegoly_obiektu_surname_wartosc.config(text=users[i].surname)
+    label_szczegoly_obiektu_miejscowosc_wartosc.config(text=users[i].location)
+    label_szczegoly_obiektu_posts_wartosc.config(text=users[i].posts)
+
+    map_widget.set_zoom(15)
+    map_widget.set_position(users[i].cordinates[0], users[i].cordinates[1])
+
 root = Tk()
 root.geometry("1200x700")
 root.title('mapbook_kt')
